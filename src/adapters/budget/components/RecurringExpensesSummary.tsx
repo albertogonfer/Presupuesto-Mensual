0c41646d3f9cd8d1ccb41345995ebf6e209cb603
albertogonfer <albertogonfer99@gmail.com>
2026-06-04 12:02:18 +0200

    fix(recurring): remove extra closing div in widget card

--- src/adapters/budget/components/RecurringExpensesSummary.tsx
+++ src/adapters/budget/components/RecurringExpensesSummary.tsx
@@ -103,7 +103,6 @@ export function RecurringExpensesSummary() {
                     </p>
                   </div>
                 </div>
-                </div>
               </div>
             )
           })}
